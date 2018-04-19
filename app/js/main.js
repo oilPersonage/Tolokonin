85d807e0f4e9f0ca9993e8fcc4deaf971f6519c4
oilPersonage <oilfreelance@ya.ru>
2018-04-19 18:08:56 +0300

    adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,42 @@
-import '../sass/main.sass'
-import '../libs/bootstrap-clockpicker.min.css'
-import '../libs/bootstrap-clockpicker.min'
-import '../libs/jquery-clockpicker.min.css'
-import '../libs/jquery-clockpicker.min'
-import '../libs/datepicker.min.css'
-import '../libs/datepicker.min'
+import '../sass/main.sass';
+import '../libs/bootstrap-clockpicker.min.css';
+import '../libs/bootstrap-clockpicker.min';
+import '../libs/jquery-clockpicker.min.css';
+import '../libs/jquery-clockpicker.min';
+import '../libs/datepicker.min.css';
+import '../libs/datepicker.min';
 
 $('.clockpicker').clockpicker({
   placement: 'top',
   align: 'left',
 });
-$('.datepicker-here').datepicker()
+$('.datepicker-here').datepicker();
 
 
-if (module.hot) {
-  module.hot.accept('./main.js', function() {
-  console.log('Accepting the updated printMe module!');
-  printMe();
-  })
-}
+const box = $('.headerNavBoxTop');
+const hamb = $('.hamburgerBox');
+const height = $('.headerNavBoxTop').height();
+
+hamb.click(() => {
+  if (Number(window.pageYOffset) < Number(height)) {
+    console.log(1);
+    $('.hamburger').toggleClass('active-menu');
+    $('.nav-container').toggleClass('active-hamburger');
+    $('.nav-box').slideToggle('ease-in-out');
+  } else {
+    $('.hamburger').toggleClass('active-menu');
+    $('.nav-container').toggleClass('active-hamburger');
+    $('.nav-box').slideToggle('ease-in-out');
+  }
+});
+
+window.onscroll = function () {
+  if (height <= window.pageYOffset + 2) {
+    $('.headerNavBox').addClass('fixedNav');
+    $('.nav-container').css({ paddingBottom: 50 });
+  } else {
+    $('.headerNavBox').removeClass('fixedNav');
+    $('.nav-container').css({ paddingBottom: 0 });
+  }
+};
+
